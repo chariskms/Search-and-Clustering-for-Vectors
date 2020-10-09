7646e8507a9409c20e460931a9aaab3668a309bc
chariskms <44468438+chariskms@users.noreply.github.com>
2020-10-09 20:18:21 +0300

    dataset input1.0

---
+++ imageVector.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdint>
+#include <cstring>
+#include <time.h>
+#include "imageVector.h"
+
+using namespace std;
+
+imageVector::imageVector(int numberOfRows,int numberOfColumns, unsigned char * buffer){
+
+    numberOfRows = numberOfRows;
+    numberOfColumns = numberOfColumns;
+
+    //pixels.resize(numberOfRows, vector<unsigned char>(numberOfColumns, 0));
+    pixels = new unsigned char*[numberOfRows];
+    for(int i = 0; i < numberOfRows;i++){
+        pixels[i] = new unsigned char[numberOfColumns];
+        for(int j = 0; j < numberOfColumns;j++){
+            pixels[i][j] = buffer[i*numberOfColumns + j];
+            
+        }   
+        cout << "done" << endl; 
+    }
+
+}
+
+imageVector::~imageVector(){
+    for(int i = 0; i < numberOfRows;i++){
+        delete[] pixels[i];
+           
+    }
+    delete[] pixels; 
+
+
+}    
--- lsh.cpp
+++ lsh.cpp
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <vector> 
 
+#include "imageVector.h"
+
+#define IMAGESIZE 800
 // ./lsh –d <input file> –q <query file> –k <int> -L <int> -ο <output file> -Ν <number of nearest> -R <radius>
 // ./lsh -d filed -R 1.0 -q fileq -k 4 -L 5 -o fileo -N 1
 
 using namespace std;
+
+
 
 int main(int argc, char** argv){
     if (argc>6 && argc<16){
         char *d=NULL, *q=NULL, *o=NULL, *k=NULL, *l=NULL, *n=NULL, *r=NULL;
         double R=1.0, exec_time;
         int K=4, L=5, N=1;
-        for (int i=0; i<argc; i++){
+        for (int i = 0; i<argc; i++){
             if (!strcmp("-d", argv[i])){
                 d = (char*)argv[i+1];                   /* -inputfile */
             }
@@ -36,6 +42,7 @@ int main(int argc, char** argv){
                 r = (char*)argv[i+1];                   /* -R */
             }
         }
+        
         if(d==NULL || q==NULL){
             cout << "You must run the program with parameters(REQUIRED): –d <input file> –q <query file>" << endl;
             cout << "With additional parameters: –k <int> -L <int> -ο <output file> -Ν <number of nearest> -R <radius>" << endl;
@@ -48,12 +55,45 @@ int main(int argc, char** argv){
             if (r!=NULL) R = atof(r);
 
             /* PROGRAM STARTS HERE */
+            
             clock_t tStart = clock();
-            cout << "Run program with parameters: ->" << d << " -> " << q << " -> " << K << " -> " << L << " -> " << o << " -> " << N << " -> " << R << endl;
+            //cout << "Run program with parameters: ->" << d << " -> " << q << " -> " << K << " -> " << L << " -> " << o << " -> " << N << " -> " << R << endl;
 
+            FILE* fileFd = fopen(d,"rb");
+            if(fileFd == NULL){
+                cout << "Wrong file input." << endl;
+                exit(0);
+            }
 
+            int magicNumber = 0,numberOfImages = 0,numberOfRows = 0,numberOfColumns = 0;
+            int numOfpixels;
+            
+            fread(&magicNumber, sizeof(int), 1, fileFd);
+            fread(&numberOfImages, sizeof(int), 1, fileFd);
+            fread(&numberOfRows, sizeof(int), 1, fileFd);
+            fread(&numberOfColumns, sizeof(int), 1, fileFd);
+            
+            cout << numberOfImages << endl;
+            cout << numberOfColumns << endl;
+            numOfpixels = numberOfRows*numberOfColumns;
 
+            unsigned char *buffer = new unsigned char[numOfpixels];
 
+            vector<imageVector *> images;
+
+            
+
+            // for(int i=0; i<numberOfImages;i++){
+
+                fread(buffer, sizeof(unsigned char), numOfpixels, fileFd);   
+                images.push_back(new imageVector(numberOfRows, numberOfColumns, buffer));
+            //}
+            
+
+            
+            delete[] buffer;
+
+            fclose(fileFd);
             /* PROGRAM ENDS HERE */
             exec_time = (double)(clock() - tStart)/CLOCKS_PER_SEC;
             cout << "Execution time is: "<< exec_time << endl;

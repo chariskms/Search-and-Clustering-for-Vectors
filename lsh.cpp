61ed64e63f7de99b7d410d7ebb9741e8d1d1c448
VasiaKoum <vasiakoum@hotmail.com>
2020-10-12 18:07:19 +0300

    add gitignore

--- lsh.cpp
+++ lsh.cpp
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <vector> 
+#include <vector>
 #include <fstream>
-#define SWAP_INT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
-
 #include "dataset.h"
 
 #define IMAGESIZE 800
+#define SWAP_INT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
+
 // ./lsh –d <input file> –q <query file> –k <int> -L <int> -ο <output file> -Ν <number of nearest> -R <radius>
-// ./lsh -d filed -R 1.0 -q fileq -k 4 -L 5 -o fileo -N 1
+// ./lsh -d train-images.idx3-ubyte -R 1.0 -q fileq -k 4 -L 5 -o fileo -N 1
 
 using namespace std;
-
-
 
 int main(int argc, char** argv){
     if (argc>6 && argc<16){
@@ -44,7 +42,7 @@ int main(int argc, char** argv){
                 r = (char*)argv[i+1];                   /* -R */
             }
         }
-        
+
         if(d==NULL || q==NULL){
             cout << "You must run the program with parameters(REQUIRED): –d <input file> –q <query file>" << endl;
             cout << "With additional parameters: –k <int> -L <int> -ο <output file> -Ν <number of nearest> -R <radius>" << endl;
@@ -57,7 +55,7 @@ int main(int argc, char** argv){
             if (r!=NULL) R = atof(r);
 
             /* PROGRAM STARTS HERE */
-            
+
             clock_t tStart = clock();
             //cout << "Run program with parameters: ->" << d << " -> " << q << " -> " << K << " -> " << L << " -> " << o << " -> " << N << " -> " << R << endl;
 
@@ -66,11 +64,11 @@ int main(int argc, char** argv){
 
             fstream input(d);
 
-            if(!input.is_open()){ 
+            if(!input.is_open()){
                 cerr<<"Failed to open input data."<<endl;
                 exit(0);
             }
-            
+
             input.read((char*)&magicNumber, 4);
             input.read((char*)&numberOfImages, 4);
             input.read((char*)&numberOfRows, 4);
@@ -81,7 +79,7 @@ int main(int argc, char** argv){
             numberOfImages = SWAP_INT32(numberOfImages);
             numberOfRows = SWAP_INT32(numberOfRows);
             numberOfColumns = SWAP_INT32(numberOfColumns);
-        
+
             cout << "magic Number: " << magicNumber << endl;
             cout << "number Of Images: " << numberOfImages << endl;
             cout << "number Of Rows: " << numberOfRows << endl;
@@ -91,12 +89,12 @@ int main(int argc, char** argv){
 
             unsigned char *buffer = new unsigned char[numOfpixels*numberOfImages];
 
-            input.read((char*)buffer, numOfpixels*numberOfImages);  
+            input.read((char*)buffer, numOfpixels*numberOfImages);
 
             Dataset dataset(magicNumber, numberOfImages, numberOfRows, numberOfColumns, buffer);
 
             input.close();
-            
+
             delete[] buffer;
 
 
